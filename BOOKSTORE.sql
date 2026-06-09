d206ff2366fba2237760f7eec2ef88d197ea8fed
Adityagaur981000 <addygaur730@gmail.com>
2026-06-09 11:07:08 +0530

    Add files via upload

---
+++ BOOKSTORE.sql
@@ -0,0 +1,148 @@
+-- Create Tables
+DROP TABLE IF EXISTS Books;
+CREATE TABLE Books (
+    Book_ID SERIAL PRIMARY KEY,
+    Title VARCHAR(100),
+    Author VARCHAR(100),
+    Genre VARCHAR(50),
+    Published_Year INT,
+    Price NUMERIC(10, 2),
+    Stock INT
+);
+DROP TABLE IF EXISTS customers;
+CREATE TABLE Customers (
+    Customer_ID SERIAL PRIMARY KEY,
+    Name VARCHAR(100),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    City VARCHAR(50),
+    Country VARCHAR(150)
+);
+DROP TABLE IF EXISTS orders;
+CREATE TABLE Orders (
+    Order_ID SERIAL PRIMARY KEY,
+    Customer_ID INT REFERENCES Customers(Customer_ID),
+    Book_ID INT REFERENCES Books(Book_ID),
+    Order_Date DATE,
+    Quantity INT,
+    Total_Amount NUMERIC(10, 2)
+)
+
+SELECT * FROM Books;
+SELECT * FROM Customers;
+SELECT * FROM Orders;
+
+-- 1) Retrieve all books in the "Fiction" genre:
+
+SELECT * FROM books 
+where genre= 'Fiction';
+
+-- 2) Find books published after the year 1950:
+SELECT* FROM books
+WHERE Published_year> 1950;
+
+-- 3) List all customers from the Canada:
+
+SELECT * FROM customers 
+WHERE country='Canada';
+
+-- 4) Show orders placed in November 2023:
+
+SELECT * FROM orders
+where order_date BETWEEN '2023-11-1' AND '2023-11-30';
+
+-- 5) Retrieve the total stock of books available:
+SELECT sum(stock) as total_stock
+FROM books;
+
+-- 6) Find the details of the most expensive book:
+SELECT * FROM books
+ORDER BY price desc
+LIMIT 1;
+
+-- 7) Show all customers who ordered more than 1 quantity of a book:
+SELECT * from orders
+where quantity>1;
+
+-- 8) Retrieve all orders where the total amount exceeds $20:
+SELECT * FROM orders
+where total_amount>20;
+
+-- 9) List all genres available in the Books table:
+SELECT DISTINCT genre 
+FROM books;
+
+-- 10) Find the book with the lowest stock:
+SELECT * from books 
+order by stock
+limit 1; 
+
+-- 11) Calculate the total revenue generated from all orders:
+SELECt SUM (total_amount) AS total_revenue
+from orders;
+
+-- Advance Questions : 
+
+-- 1) Retrieve the total number of books sold for each genre:
+SELECT b.genre , SUM(o.quantity) AS total_book_sold
+FROM orders o
+JOIN books b ON o.book_ID = b.book_ID
+GROUP BY genre;
+
+-- 2) Find the average price of books in the "Fantasy" genre:
+SELECT avg(price) as avg_p 
+FROM books
+WHERE genre= 'Fantasy'
+
+-- 3) List customers who have placed at least 2 orders:
+SELECT o.customer_id, c.name, COUNT(o.order_id)AS order_count
+from orders o
+JOIN customers c ON o.customer_id=c.customer_id
+GROUP BY o.customer_id , c.name
+HAVING COUNT(order_id)>=2;
+
+-- 4) Find the most frequently ordered book:
+SELECT o.book_id,b.title,count(o.order_id) AS ORDER_COUNT
+FROM orders o
+JOIN books b ON o.book_id=b.book_id
+GROUP BY o.book_id,b.title
+ORDER BY ORDER_COUNT desc LIMIT 2;
+
+-- 5) Show the top 3 most expensive books of 'Fantasy' Genre :
+SELECT * from books
+WHERE genre= 'Fantasy'
+ORDER BY Price desc LIMIT 3;
+
+-- 6) Retrieve the total quantity of books sold by each author:
+SELECT b.author,SUM(o.quantity) as TOTAL_QUANTITY
+FROM orders o
+JOIN books b ON b.book_id=o.book_id
+GROUP BY b.author
+ORDER BY TOTAL_QUANTITY;
+
+-- 7) List the cities where customers who spent over $30 are located:
+SELECT c.city, c.country, o.total_amount 
+FROM orders o
+JOIN customers c ON c.customer_id= o.customer_id
+WHERE o.total_amount>30
+ORDER BY total_amount desc;
+
+-- 8) Find the customer who spent the most on orders:
+SELECT c.customer_id, c.name, SUM(o.total_amount) AS TOTAL_AMOUNT
+FROM orders o
+JOIN customerS c ON c.customer_id=o.customer_id
+GROUP BY c.name , c.customer_id
+ORDER BY TOTAL_AMOUNT DESC ;
+
+--9) Calculate the stock remaining after fulfilling all orders:
+SELECT b.book_id,b.title, b.stock, COALESCE(sum(o.quantity),0) AS ORDER_QUANTITY,
+b.stock- COALESCE(sum(o.quantity),0) AS REMAINING_STOCK
+FROM books b
+LEFT JOIN orders o ON b.book_id=o.book_id
+GROUP BY b.book_id
+ORDER BY b.book_id;
+
+select * from customers
+select * from orders
+select * from books
+
